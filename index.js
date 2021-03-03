daed337f993ac6c54a396c3823375884e8079aec
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-03-03 19:21:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// const event = require('events');
+// const fs = require('fs').promises;
+// const eventEmitter = new event.EventEmitter();
+
+// eventEmitter.on('lunch', () => {
+//   console.log('eat!');
+// });
+// console.log('start!');
+// eventEmitter.emit('lunch');
+// console.log('end!');
+// const file = async () => {
+//   const data = await fs.readFile('./hello.txt', 'utf-8');
+//   console.log(data);
+// };
+// file();
+import express from 'express';
+import fs from 'fs/promises';
+import { readFile } from 'node:fs/promises';
+const app = express();
+app.get('/', async (_, res) => {
+  try {
+    const data = await fs.readFile('./index.html', 'utf-8');
+    res.send(data);
+  } catch (error) {
+    if (error) res.status(500).send('Sorry, something was wrong 😰 !');
+  }
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server listen on port ', PORT);
+});
